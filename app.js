036af4db8c6392c8ca3b97789efc3f95d3e37a5b
LampLighterexe <94939349+LampLighterexe@users.noreply.github.com>
2022-01-11 07:46:15 -0700

    Update app.js
    added some more of the code

--- app.js
+++ app.js
@@ -1,19 +1,70 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
 const PREFIX = '*';
+const { joinVoiceChannle } = require('@discordjs/voice');
+/*const connection = joinVoiceChannle({
+  channelId: 927590527950553168,
+  guildId: 927590080623804416,
+  adapterCreator: channel.guild.voiceAdapterCreator,
+  });*/
 
-// read read UIDs from .env file
+
+
 const dotenv = require('dotenv');
 dotenv.config();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+   client.user.setActivity(' Vine Thunds', { type: 'LISTENING' });
+    });
+
+//PLAYING: WATCHING: LISTENING: STREAMING:
+client.on('message', message => {
+  //if(message.content == "Among Us" || "among us"){
+     // message.channel.send('you are sus')
+  //}
+  function thequent(){
+  message.channel.send('<@376176320561938434>');
+  }
+    
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+  if (message.author.bot) return;
+
+  if(message.content == 'cs'){
+    message.channel.send('cringe')
+  }
+
+    switch(args[0]){
+      case 'help':
+      message.channel.send('Current comands and features: ping: will return a string. funny: will return an image. voise: will join a voice call (noise coming soon). *more to come eventually.* ')
+      break;
+      case 'ping':
+        message.channel.send('*vine boom*');
+        break;
+      case 'funny':
+        message.channel.send({
+          files: ['https://i.ytimg.com/vi/gHw_VxC7iFc/maxresdefault.jpg']
+        });
+        break;
+      case 'voice':
+        const streamOptions = { seek: 0, volume: 1 };
+        var voiceChannel = message.member.voiceChannel;
+        if(!message.member.voice.channel) return message.channel.send("🖕 Get in a call"); //If you are not in the voice channel, then return a message
+        //message.member.voice.channel.join().then(connection => {
+        //});
+        break;
+      case 'home' :
+        for(var i =0; i < 5; i++){
+          thequent();
+        }
+      }
+    
 });
 
-client.on('message', msg => {
-
-
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 client.login(process.env.token);
-package.json;
